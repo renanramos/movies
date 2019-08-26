f1bd29f73cc87d6634fad0dfb24cbf5cdcba739c
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2019-08-26 20:53:57 -0300

    Incluída listagem dos filmes mais populares

--- src/app/movie/movies-list/movies-list.component.ts
+++ src/app/movie/movies-list/movies-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MoviesService } from '../movies.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css']
 })
-export class MoviesListComponent implements OnInit {
+export class MoviesListComponent implements OnInit, OnDestroy {
+ 
+  movies: [];
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private moviesService: MoviesService
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.moviesService.getPopularMovies()
+      .subscribe((movies: any) => {
+        this.movies = movies;
+      });
   }
-
+  
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
---
+++ src/app/movie/movies.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  private API_KEY = environment.API_KEY;
+  private baseUrl = environment.baseUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private withBaseUrl(path: string) {
+    return `${this.baseUrl}${path}?api_key=${this.API_KEY}`;
+  }
+
+  getPopularMovies() {
+    return this.http.get(this.withBaseUrl('movie/popular'))
+    .pipe(
+      map((response: any) => response.results)
+    );
+  }
+
+}
